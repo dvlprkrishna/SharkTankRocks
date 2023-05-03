1640389bf6761468aa913e49e19d0513b867ffb3
dvlprkrishna <dvlprkrishna@gmail.com>
2023-05-04 00:54:04 +0530

    Initial Page Setup / Styling

---
+++ src/pages/Header.jsx
@@ -0,0 +1,54 @@
+import "../assets/css/Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="navStripe">
+            <p>list of Invesments made in Shark Tank India </p>
+            <a href="#">About Us</a>
+          </div>
+          <div className="filterWrap">
+            <div>
+              <h2>Find your favorite deal below</h2>
+            </div>
+            <div className="filter">
+              <button>
+                Season <span>1</span>
+              </button>
+              <button>
+                Season <span>2</span>
+              </button>
+            </div>
+            <div>
+              <button>
+                Namita Thapar<span>1</span>
+              </button>
+              <button>
+                Ashneer Grover <span>2</span>
+              </button>
+              <button>
+                Vineeta <span>2</span>
+              </button>
+              <button>
+                Aman Gupta <span>2</span>
+              </button>
+              <button>
+                Peyush Bansal <span>2</span>
+              </button>
+              <button>
+                Ghazal Alag <span>2</span>
+              </button>
+              <button>
+                Anupam Mittal <span>2</span>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
