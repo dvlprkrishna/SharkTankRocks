5fb10538ae2950916f51d7cff3d4b2fae5e507af
dvlprkrishna <dvlprkrishna@gmail.com>
2023-05-05 03:00:33 +0530

    Basic listing

--- src/pages/MainList.jsx
+++ src/pages/MainList.jsx
@@ -19,44 +19,37 @@ const MainList = (props) => {
           <img src={viteLogo} alt="" />
           <div className="investors">
             <img
-              src={ashneer}
+              src={props.elem.Ashneer == "Deal" ? `${ashneer}` : null}
               className="logo react"
               width={36}
               alt="ashneer"
             />
-            <img src={anupam} className="logo react" width={36} alt="anupam" />
-            <img
-              src={vineeta}
-              className="logo react"
-              width={36}
-              alt="vineeta"
-            />
-            <img src={namita} className="logo react" width={36} alt="namita" />
-            <img src={ghazal} className="logo react" width={36} alt="ghazal" />
-            <img src={peyush} className="logo react" width={36} alt="peyush" />
           </div>
         </div>
         <div className="pitchInfo">
           <div>
-            <small className="episodeNumber">S1 E06 p016</small>
+            <small className="episodeNumber">
+              {"S01 " + props.elem.Episode + ""}
+              {/* {"S01 " + props.elem["EpNo"] + " " + props.elem["Pitch No"]} */}
+            </small>
             <h3>
               {props.elem.Brand.length > 16
                 ? props.elem.Brand.substring(0, 16) + ".."
                 : props.elem.Brand}
             </h3>
-            <p>Idea</p>
+            <p>{props.elem.Idea}</p>
             <div className="askingPrice">
               <img src={askLogo} alt="" width={32} />
               <div>
                 <small>Asking Price</small>
-                <p>₹45 lakhs for 5% equity</p>
+                <p>{props.elem["Original ask"]}</p>
               </div>
             </div>
             <div className="deal">
               <img src={dealLogo} alt="" width={32} />
               <div>
                 <small>Deal</small>
-                <p>₹1 crore for 15% equity</p>
+                <p>{props.elem["Deal"]}</p>
               </div>
             </div>
           </div>
