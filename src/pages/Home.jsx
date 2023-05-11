bdc34afaf58f9d3610e2323d6c3207f06fbc8e8d
dvlprkrishna <dvlprkrishna@gmail.com>
2023-05-11 19:41:16 +0530

    Updated Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import github from "/github.png";
+import twitter from "/twitter.png";
 
 import "../App.css";
 import Header from "./Header";
@@ -47,10 +47,38 @@ const Home = () => {
         {displayCount <= currentSeason.length ? "Load More" : "All Deal Loaded"}
       </button>
       <footer>
+        <div style={{ textAlign: "left" }}>
+          <small>
+            &copy; All trademarks, logos and brand names are the property of
+            their respective owners. <br />
+          </small>
+          <small>
+            &copy; {new Date().getFullYear()} All Right Reserved. Created By -
+            <a
+              href="https://krishnasahu.in"
+              rel="noreferrer"
+              target="_blank"
+              style={{ marginLeft: "0.25em" }}
+            >
+              Krishna Sahu
+            </a>
+          </small>
+        </div>
         <p>
-          &copy; {new Date().getFullYear()}. Created By{" "}
-          <a href="https://krishnasahu.in" target="_blank" rel="noreferrer">
-            - Krishna Sahu
+          <a
+            href="https://twitter.com/dvlprkrishna/"
+            target="_blank"
+            rel="noreferrer"
+            style={{ marginRight: "1em" }}
+          >
+            <img src={twitter} width={30} alt="" />
+          </a>
+          <a
+            href="https://github.com/dvlprkrishna/SharkTankRocks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} width={30} alt="" />
           </a>
         </p>
       </footer>
